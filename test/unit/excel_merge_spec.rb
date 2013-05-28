5d0870d03117b71d756b72bcfb356f8c45375762
Sean Dunn <sd9@sanger.ac.uk>
2013-05-28 18:10:40 +0100

    Examples of before files for merging (Also completed manifest csv - to be removed)

---
+++ test/unit/excel_merge_spec.rb
@@ -0,0 +1,11 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+require File.expand_path(File.dirname(__FILE__) + "../../../lib/excel_merge")
+
+
+describe ExcelMerge do
+  it "merges an Excel template and a CSV data file" do
+    puts "runs"
+  end
+end
