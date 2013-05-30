e9b2626f4ad31042a66a09006d50597183ddeba7
Sean Dunn <sd9@sanger.ac.uk>
2013-05-30 15:17:08 +0100

    Look up columns in template

--- lib/excel_merge.rb
+++ lib/excel_merge.rb
@@ -1,25 +1,27 @@
-require "spreadsheet"
-require "csv"
+require 'spreadsheet'
+require 'csv'
 
 
 class ExcelMerge
-  TEMPLATE_OFFSET = 9
+  TEMPLATE_HEADER_ROW = 8
+  TEMPLATE_OFFSET     = 9  # Point at which we insert into the template
 
   class << self
-
     def merge_template_with_csv(template, csv)
-      template = Spreadsheet.open(template)
-      csv      = CSV.parse(csv)
-
-      worksheet = template.worksheets.first
-
-      headers = csv.shift
+      template    = Spreadsheet.open(template)
+      worksheet   = template.worksheets.first
+      xls_headers = worksheet.row(TEMPLATE_HEADER_ROW).to_a.map {|c| c.downcase unless c.nil? }
+      csv         = CSV.parse(csv)
+      csv_headers = csv.shift.map(&:downcase)
 
       csv.each_with_index do |csv_row, row_index|
-
         csv_row.each_with_index do |csv_cell, col_index|
           next if csv_cell.nil?
-          worksheet[row_index + TEMPLATE_OFFSET, col_index] = csv_cell
+
+          xls_col = xls_headers.find_index(csv_headers[col_index]) or
+            raise 'CSV column not found in template.'
+
+          worksheet[row_index + TEMPLATE_OFFSET, xls_col] = csv_cell
         end
       end
 
