969e84b992da256039ed25a8e1b6db55333471c5
Sean Dunn <sd9@sanger.ac.uk>
2013-06-03 12:08:55 +0100

    Move service URL to /excel_merge

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require File.expand_path(File.dirname(__FILE__) +'/../lib/excel_merge')
 require 'tempfile'
 
-post '/' do
+post '/merge_excel/' do
   xls_template = params['template'][:tempfile]
   samples_csv  = params['manifest-details'][:tempfile]
   manifest     = ExcelMerge.merge_template_with_csv(xls_template, samples_csv)
